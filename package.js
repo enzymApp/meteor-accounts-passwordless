10c9ae0270be5cb7b7d692543e53e0c8600b854e
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-06-06 09:37:18 +0200

    publish package

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: 'enzym:accounts-passwordless',
+  name: 'enzymapp:accounts-passwordless',
   version: '0.3.1',
   summary: 'Token-based one-time password (OTPW) authentication (nopassword, passwordless)',
-  git: 'https://github.com/efounders/meteor-accounts-passwordless',
+  git: 'https://github.com/enzymapp/meteor-accounts-passwordless',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.6.1');
 
-  api.use(['ecmascript', 'tmeasday:check-npm-versions'])
-  api.use(['tracker', 'underscore', 'templating', 'session'], 'client');
+  api.use(['ecmascript', 'tmeasday:check-npm-versions@0.3.2'])
+  api.use(['tracker', 'underscore', 'templating@1.3.2', 'session'], 'client');
   api.use('email', 'server');
   api.use(['accounts-base', 'check'], ['client', 'server']);
   api.use(['random', 'jperl:match-ex@1.0.0'], 'server')
