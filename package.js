a4342c2ebcd8e7d339a31a5e049baca8cd4782ec
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-06-21 10:01:58 +0200

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'enzymapp:accounts-passwordless',
-  version: '0.3.4',
+  version: '0.3.5',
   summary: 'Token-based one-time password (OTPW) authentication (nopassword, passwordless)',
   git: 'https://github.com/enzymapp/meteor-accounts-passwordless',
   documentation: 'README.md'
