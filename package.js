3470678c4bec98fbfd5a98b567eb0c6ade78cb4b
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-09-12 09:46:54 +0200

    replace persistent-session package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'enzymapp:accounts-passwordless',
-  version: '0.3.8',
+  version: '0.3.9',
   summary: 'Token-based one-time password (OTPW) authentication (nopassword, passwordless)',
   git: 'https://github.com/enzymapp/meteor-accounts-passwordless',
   documentation: 'README.md'
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
 
   api.use(['ecmascript', 'tmeasday:check-npm-versions@0.3.2'])
   api.use([
-    'tracker', 'templating@1.3.2', 'session', 'u2622:persistent-session@0.4.4'
+    'tracker', 'templating@1.3.2', 'session', 'cultofcoders:persistent-session'
   ], 'client')
   api.use('email', 'server')
   api.use(['accounts-base', 'check'], ['client', 'server'])
